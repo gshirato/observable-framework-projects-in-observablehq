a28b591eba249e8c39d0d930dc19dbe04e1cca42
Gota Shirato / 白戸豪大 <gshirato@uni-bonn.de>
2024-05-23 16:41:28 +0200

    add: getAttr to set default values

--- src/chart/components/Text.js
+++ src/chart/components/Text.js
@@ -1,16 +1,19 @@
+import getAttr from './getAttr.js';
+
 export default function drawDots(sel, config) {
     sel
         .selectAll('text')
         .data(config.data)
         .join('text')
-        .attr('x', config.x)
-        .attr('y', config.y)
-        .attr('fill', config.fill)
-        .attr('stroke', config.stroke)
-        .attr('opacity', config.opacity)
-        .attr("text-anchor", config['text-anchor'])
-        .attr("font-size", config['font-size'])
-        .attr("font-weight", config['font-weight'])
-        .attr('pointer-events', config['pointer-events'])
-        .text(config.text)
+        .attr('x', getAttr(config, 'x'))
+        .attr('y', getAttr(config, 'y'))
+        .attr('fill', getAttr(config, 'fill', 'black'))
+        .attr('stroke', getAttr(config, 'stroke', 'none'))
+        .attr('opacity', getAttr(config, 'opacity', 1))
+        .attr("text-anchor", getAttr(config, 'text-anchor', 'middle'))
+        .attr("font-size", getAttr(config, 'font-size', '12px'))
+        .attr("font-weight", getAttr(config, 'font-weight', 'normal'))
+        .attr("font-family", getAttr(config, 'font-family', 'sans-serif'))
+        .attr('pointer-events', getAttr(config, 'pointer-events', 'none'))
+        .text(getAttr(config, 'text', ''))
 }
