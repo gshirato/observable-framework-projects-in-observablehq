aad6eabdb573c2427638db1dc4c551797bf6c295
Gota Shirato / 白戸豪大 <gshirato@uni-bonn.de>
2024-05-23 17:04:53 +0200

    comment out

--- src/concast/data/meta/concast/concast-episodes.csv.py
+++ src/concast/data/meta/concast/concast-episodes.csv.py
@@ -11,15 +11,15 @@ def split_number(number):
     return split[0], split[1]
 
 
-files = []
-# TODO: use sys.~~~
-for filename in glob.glob("./src/data/concast/meta/concast/*.json"):
-    with open(filename, "r") as f:
-        files.append(json.load(f))
+# files = []
+# # TODO: use sys.~~~
+# for filename in glob.glob("./src/data/concast/meta/concast/*.json"):
+#     with open(filename, "r") as f:
+#         files.append(json.load(f))
 
-df = pd.DataFrame(files)
-df["main-number"] = df["Number"].apply(lambda x: int(split_number(x)[0]))
-df["sub-number"] = df["Number"].apply(lambda x: split_number(x)[1])
-df = df.sort_values(by="main-number", ascending=True)
-df = df.reset_index(drop=True)
-df.to_json(sys.stdout, orient="records")
+# df = pd.DataFrame(files)
+# df["main-number"] = df["Number"].apply(lambda x: int(split_number(x)[0]))
+# df["sub-number"] = df["Number"].apply(lambda x: split_number(x)[1])
+# df = df.sort_values(by="main-number", ascending=True)
+# df = df.reset_index(drop=True)
+# df.to_json(sys.stdout, orient="records")
