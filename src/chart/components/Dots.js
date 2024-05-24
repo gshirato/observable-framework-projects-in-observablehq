fdfaa4b1f8d9c0069a4aac667ac1a26a0caed4f9
Gota Shirato / 白戸豪大 <gshirato@uni-bonn.de>
2024-05-24 12:26:04 +0200

    add: width and dasharray

--- src/chart/components/Dots.js
+++ src/chart/components/Dots.js
@@ -10,5 +10,7 @@ export default function drawDots(sel, config) {
         .attr('r', getAttr(config, 'r', 1))
         .attr('fill', getAttr(config, 'fill', 'black'))
         .attr('stroke', getAttr(config, 'stroke', 'none'))
+        .attr('stroke-width', getAttr(config, 'stroke-width', 1))
+        .attr('stroke-dasharray', getAttr(config, 'stroke-dasharray', ''))
         .attr('opacity', getAttr(config, 'opacity', 1))
 }
