346c29bd6da6e9446925c3e7198ef3e4657d0c4b
Gota Shirato / 白戸豪大 <gshirato@uni-bonn.de>
2024-06-04 15:13:57 +0200

    add: circles

--- src/data_investigation/components/umap/chart.js
+++ src/data_investigation/components/umap/chart.js
@@ -6,6 +6,7 @@ import GeneralChart from "../../../chart/components/GeneralChart.js";
 export default class UmapChart extends GeneralChart {
     constructor(data, selector, config) {
         super(data, selector, config);
+        this.config = config;
         this.setAxes();
     }
 
@@ -35,12 +36,27 @@ export default class UmapChart extends GeneralChart {
             .call(yAxis)
     }
 
+    drawPoints(sel) {
+        sel.selectAll("circle")
+            .data(this.data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => this.sx(d[0]))
+            .attr("cy", d => this.sy(d[1]))
+            .attr("r", 2)
+            .attr("fill", "black")
+            .on("mouseover", _.partial(this.mouseover, this))
+            .on("mousemove", _.partial(this.mousemove, this))
+            .on("mouseleave", _.partial(this.mouseleave, this))
+    }
 
     draw() {
-
         this.svg.call(this.drawAxes.bind(this));
+        this.svg.call(this.drawPoints.bind(this));
         this.svg.call(this.drawTitle.bind(this));
     }
+
+
 
     drawTitle(sel) {
     }
