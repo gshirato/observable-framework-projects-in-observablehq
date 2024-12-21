513f9ca9b4002a3b5d23ef7ec707b7a8e9af5f9e
Gota Shirato / 白戸豪大 <gshirato@uni-bonn.de>
2024-12-21 13:34:49 +0100

    update: prediction 2024 j1

--- src/predictor-j1/components/TableChart.js
+++ src/predictor-j1/components/TableChart.js
@@ -7,12 +7,24 @@ class TableChart {
       this.data = data;
       this.selector = selector;
       this.config = config;
+      this.nTeams = Object.keys(this.data[0]).length - 1;
+      this.differences = calculateDifferences(this.data);
+      this.differences = this.sortData(this.differences);
+      console.log(this.differences)
       this.setAxis();
       this.createTable();
     }
 
+    sortData(data) {
+      return data.sort((a, b) => d3.ascending(d3.sum(Object.values(a['diff'])), d3.sum(Object.values(b['diff']))));
+    }
+
+    score(raw) {
+      return d3.sum(Object.values(raw['diff']));
+    }
+
     setAxis() {
-      this.sc = d3.scaleLinear().domain([0, 17]).range(["white", "#de2d26"]);
+      this.sc = d3.scaleLinear().domain([0, this.nTeams - 1]).range(["white", "#de2d26"]);
     }
 
     createTable() {
@@ -31,35 +43,41 @@ class TableChart {
       // Add header row
       const headerRow = this.table.append("tr");
       headerRow.append("th").text("予想者");
-      for (let i = 1; i <= 18; i++) {
+      for (let i = 1; i <= this.nTeams; i++) {
         headerRow.append("th").text(i);
       }
+      headerRow.append("th").text("スコア");
 
+      const thisClass = this;
       // Add data rows
-      this.data.forEach((item) => {
-        const row = this.table.append("tr");
-        row.append("td").style("font-weight", "bold").text(item.予想);
-        for (let i = 1; i <= 18; i++) {
-          row.append("td").text(item[i]);
-        }
-      });
+
+      this.differences.forEach((item) => {
+        console.log(item)
+          const row = this.table.append("tr");
+          row.append("td").style("font-weight", "bold").text(item.予想);
+          for (let i = 1; i <= this.nTeams + 1; i++) {
+            row.append("td").text(item[i]);
+          }
+          // sum
+          const differences = Object.values(thisClass.differences.find(d=>d.予想 === item.予想)['diff']);
+          row.append('td').text(d3.sum(differences));
+        });
     }
 
     colorCells() {
-      const differences = calculateDifferences(this.data);
 
       this.table
         .selectAll("tr:not(:first-child)")
-        .data(differences)
+        .data(this.differences)
         .selectAll("td:not(:first-child)")
         .data((d) => Object.values(d["diff"]))
         .style("background-color", (d, i) => this.sc(d));
 
       this.table
         .selectAll("tr:nth-child(2)")
-        .data(differences)
+        .data(this.differences)
         .selectAll("td")
-        .data(d3.range(19))
+        .data(d3.range(this.nTeams + 1))
         .style("background-color", "#def");
     }
 
--- src/predictor-j1/components/utils.js
+++ src/predictor-j1/components/utils.js
@@ -1,17 +1,19 @@
 export function calculateDifferences(data) {
   // Assuming '結果' is the key for the actual results in your data
+    const nTeams = Object.keys(data[0]).length - 1;
     const differences = {};
     const actualResults = data.find((item) => item.予想 === "結果");
-    for (let j = 1; j <= 18; j++) {
+    for (let j = 1; j <= nTeams; j++) {
       differences[actualResults[j]] = j;
     }
 
+
     const res = data.map((item) => {
       const diff = {};
-      for (let i = 1; i <= 18; i++) {
+      for (let i = 1; i <= nTeams; i++) {
         const actualPos = findKeyByValue(actualResults, item[i]);
         diff[actualPos] = Math.abs(i - differences[item[i]]);
-        console.log(diff)
+        // console.log('aa:', diff, actualPos, item[i], differences[item[i]], actualResults)
       }
 
       return { ...item, diff };
